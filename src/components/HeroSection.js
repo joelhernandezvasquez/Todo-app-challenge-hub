259dca0e9f1c89a4ad302d9d697c5271ceb07e86
joelhernandezvasquez <jhernandez98@outlook.es>
2021-12-30 13:06:27 -0500

    starting

---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import '../styles/main.scss';
+
+const HeroSection = () => {
+    return (
+        <div className='hero-section'>
+            
+        </div>
+    )
+}
+
+export default HeroSection;
